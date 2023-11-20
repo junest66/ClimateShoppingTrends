f5f8dffc2407615c80dc70b05dfa8f281abc3457
junest66 <chlwndks333@gmail.com>
2023-11-20 23:20:58 +0900

    feat: model에 넘겨줄 데이터에 현재시각을 매핑한 값 추가

--- utill.py
+++ utill.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+
+# form에서 받은 데이터를 한국어로 변환하는 함수
 def translate_to_korean(data_list):
     korean_data = {
         "age": {
@@ -24,3 +28,30 @@ def translate_to_korean(data_list):
             korean_user_info[key] = value
 
     return korean_user_info
+
+
+# 현재 시간을 숫자로 매핑하는 함수
+def map_time_to_number():
+    current_hour = datetime.now().hour
+
+    # 시간 범위에 따라 숫자 매핑
+    if 0 <= current_hour < 6:
+        return 1
+    elif 6 <= current_hour < 8:
+        return 2
+    elif 8 <= current_hour < 10:
+        return 3
+    elif 10 <= current_hour < 12:
+        return 4
+    elif 12 <= current_hour < 14:
+        return 5
+    elif 14 <= current_hour < 16:
+        return 6
+    elif 16 <= current_hour < 18:
+        return 7
+    elif 18 <= current_hour < 20:
+        return 8
+    elif 20 <= current_hour < 22:
+        return 9
+    elif 22 <= current_hour < 24:
+        return 10
