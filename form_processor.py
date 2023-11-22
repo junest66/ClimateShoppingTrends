5364acf80172a386400147a93c0d9dcd3ca41b15
junest66 <chlwndks333@gmail.com>
2023-11-23 01:09:19 +0900

    refacotr: 가맹점크롤링 필터 목록 수정

--- form_processor.py
+++ form_processor.py
@@ -17,14 +17,14 @@ def form():
 @app.route("/submit", methods=["POST"])
 def submit():
     model_data = {
-        "apr_tizn_c": map_time_to_number(), # 시간
-        "province": request.form.get("currentProvince"), # 광역시도
-        "district": request.form.get("currentDistrict"), # 시군구
-        "gender": request.form.get("gender"), # 성별
-        "age": request.form.get("age"), # 나이대 그룹
-        "married": request.form.get("married"), # 결혼 여부
-        "children": request.form.get("children"), # 자녀 보유 여부
-        "influx": request.form.get("influx") # 거주지 일치 여부
+        "apr_tizn_c": map_time_to_number(),  # 시간
+        "province": request.form.get("currentProvince"),  # 광역시도
+        "district": request.form.get("currentDistrict"),  # 시군구
+        "gender": request.form.get("gender"),  # 성별
+        "age": request.form.get("age"),  # 나이대 그룹
+        "married": request.form.get("married"),  # 결혼 여부
+        "children": request.form.get("children"),  # 자녀 보유 여부
+        "influx": request.form.get("influx"),  # 거주지 일치 여부
     }
 
     # 데이터를 모델에 전달하고 결과를 받습니다.
@@ -33,11 +33,11 @@ def submit():
     # 가맹점 추천용 폼 데이터
     franchise_info = {
         "car": request.form.get("car"),  # 주차여부 필터
-        "household": request.form.get("household"),  # 1인 = 혼밥필터, 1이상 = 가족모임 필터
-        "pet": request.form.get("pet"),  # 애견동반 필터
-        "partner": request.form.get("partner"),  # 목적: 데이트필터
+        # "household": request.form.get("household"),  # 1인 = 혼밥필터, 1이상 = 가족모임 필터
+        # "pet": request.form.get("pet"),  # 애견동반 필터
+        # "partner": request.form.get("partner"),  # 목적: 데이트필터
         "preference": request.form.get("general_preference"),  # 정렬 필터
-        "ambiance": request.form.getlist("ambiance_preferences"),  # 분위기 필터
+        "ambiance": request.form.get("ambiance_preferences"),  # 분위기 필터
     }
 
     # 사용자 정보를 한글로 변환합니다.
